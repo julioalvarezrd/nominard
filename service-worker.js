9ad4f6b8b32b57c9941254f31bba979d285cde4c
Julio Alvarez <julioalvarezrd@gmail.com>
2023-11-11 16:11:23 -0400

    WPA

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,24 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-            console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(error => {
-            console.error('Error al registrar el Service Worker:', error);
-        });
-}
+const cacheName = 'nominard-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/img/logo.svg'
+];
 
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
