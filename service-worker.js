7620b1df7ff687df8f2532b0040dd7009edc5314
Julio Alvarez <julioalvarezrd@gmail.com>
2023-11-11 15:48:48 -0400

    WPA

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch(error => {
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  }
+</script>
